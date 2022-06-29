148218958408994cc0b05c40d8d82214f5977fa7
SneakyTurtle <le741996cf@gmail.com>
2022-06-29 11:28:28 +0700

    create basically video section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
+/**
+ * Author: Sneaky
+ * Date Created: 28/06/2022
+ * Count up number
+ */
 $(".counter").countUp({
     time: 2000,
     delay: 10,
+});
+
+/**
+ * Author: Sneaky
+ * Date Created: 29/06/2022
+ * Mafnific popup video
+ */
+$(".open--video--popup").magnificPopup({
+    type: "inline",
+    midClick: true,
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: "auto",
+
+    closeBtnInside: true,
+    preloader: false,
+
+    removalDelay: 300,
 });
